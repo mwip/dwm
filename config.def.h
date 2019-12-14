7dacdf6d942b48cb57f7d0bffde18b74856c4802
mwip <matthias.weigand@protonmail.com>
2019-12-14 22:33:55 +0100

    center

--- config.def.h
+++ config.def.h
@@ -32,10 +32,10 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            0,           -1 },
-	{ "Firefox",  NULL,       NULL,       0,            0,           -1 },
-	{ "KeePassXC",NULL,       NULL,       0,            0,           -1 },
+	/* class      instance    title       tags mask	    iscentered   isfloating   monitor */
+	{ "Gimp",     NULL,       NULL,       0,            0,           0,           -1 },
+	{ "Firefox",  NULL,       NULL,       0,            0,           0,           -1 },
+	{ "KeePassXC",NULL,       NULL,       0,            0,           0,           -1 },
 };
 
 /* layout(s) */
