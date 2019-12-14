5cd0fff16e3b33ab5e2c818b6625036b1a88fbed
mwip <matthias.weigand@protonmail.com>
2019-12-14 15:28:40 +0100

    config

--- config.def.h
+++ config.def.h
@@ -28,9 +28,9 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
-	{ "KeePassXC",NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Gimp",     NULL,       NULL,       0,            0,           -1 },
+	{ "Firefox",  NULL,       NULL,       0,            0,           -1 },
+	{ "KeePassXC",NULL,       NULL,       0,            0,           -1 },
 };
 
 /* layout(s) */
