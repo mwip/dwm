99008bbf6ec2d4eb27969fb06d254369f36c099e
mwip <matthias.weigand@protonmail.com>
2020-02-05 17:41:54 +0100

    switching to alacritty

--- config.def.h
+++ config.def.h
@@ -67,7 +67,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "/home/loki/.scripts/dmenu_recent.sh", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "urxvtc", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
 static const char *browsercmd[] = { "firefox", NULL };
 static const char *brightness_up[]  =   { "/home/loki/.scripts/brightness.sh", "+", NULL };
 static const char *brightness_down[]  = { "/home/loki/.scripts/brightness.sh", "-", NULL };
